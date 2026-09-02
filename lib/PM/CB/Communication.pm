fc765e159accb7ca86673d7d70bdab6c9f052b04
E. Choroba <choroba@matfyz.cz>
2026-09-02 13:23:30 +0200

    Add timestamps to warnings

--- lib/PM/CB/Communication.pm
+++ lib/PM/CB/Communication.pm
@@ -7,7 +7,7 @@ use Encode;
 use Time::HiRes;
 use WWW::Mechanize;
 use XML::LibXML;
-use PM::CB::Common qw{ to_entities };
+use PM::CB::Common qw{ to_entities tswarn };
 
 use constant {
     FREQ             => 10,
@@ -104,7 +104,7 @@ sub communicate {
 
                 $previous = $xml;
             } else {
-                warn "PMCB: $@";
+                tswarn($@);
             }
         }
 
@@ -129,7 +129,7 @@ sub get_monklist {
             $self->get_monklist($repeat + 1);
 
         } else {
-            warn "PMCB: Can't get monklist.\n";
+            tswarn("Can't get monklist.");
         }
         return
     }
